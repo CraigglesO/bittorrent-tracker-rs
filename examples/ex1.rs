a265669c593feda585809202ba76ef4111143d14
Craig OConnor <oconnorct1@gmail.com>
2017-05-26 01:08:26 -0400

    client updates

--- examples/ex1.rs
+++ examples/ex1.rs
@@ -1,10 +1,11 @@
 extern crate bittorrent_tracker;
 
-use Client;
+use bittorrent_tracker::Client;
 
 fn main() {
-    let client = Client::new("udp", "0123456789012345678901234567890123456789", "zer0day.ch:1337");
+    let client = Client::new("udp", "0123456789012345678901234567890123456789", "0.0.0.0:1337");
 
-    let x = client.connection_request();
-    println!("responce: {:?}", x);
+    let x = client.scrape();
+
+    println!("scrape: {:?}", x);
 }
--- src/client.rs
+++ src/client.rs
@@ -1,49 +1,155 @@
 #![allow(dead_code)]
+#![allow(unsed_imports)]
+extern crate byteorder;
 
+use std::error::Error;
+use std::io::Cursor;
+use self::byteorder::{BigEndian, ReadBytesExt};
+
+use std::thread;
+use std::sync::mpsc::channel;
 use std::mem::transmute;
 
-use std::net::UdpSocket;
-use std::net::{SocketAddr, SocketAddrV4};
+use std::net::{UdpSocket, Ipv4Addr, SocketAddr, SocketAddrV4};
 
-const PROTOCOL_ID:     &'static [u8] = [0x00, 0x00, 0x04, 0x17, 0x27, 0x10, 0x19, 0x80];
-const ACTION_CONNECT:  &'static [u8] = [0x00, 0x00, 0x00, 0x00];
-const ACTION_ANNOUNCE: &'static [u8] = [0x00, 0x00, 0x00, 0x01];
-const ACTION_SCRAPE:   &'static [u8] = [0x00, 0x00, 0x00, 0x02];
-const ACTION_ERROR:    &'static [u8] = [0x00, 0x00, 0x00, 0x03];
+const PROTOCOL_ID:     &'static [u8] = &[0x00, 0x00, 0x04, 0x17, 0x27, 0x10, 0x19, 0x80];
+const ACTION_CONNECT:  &'static [u8] = &[0x00, 0x00, 0x00, 0x00];
+const ACTION_ANNOUNCE: &'static [u8] = &[0x00, 0x00, 0x00, 0x01];
+const ACTION_SCRAPE:   &'static [u8] = &[0x00, 0x00, 0x00, 0x02];
+const ACTION_ERROR:    &'static [u8] = &[0x00, 0x00, 0x00, 0x03];
 
+#[derive(Debug)]
+pub struct Peer {
+    pub ip: Ipv4Addr,
+    pub port: u16,
+}
+
+#[derive(Debug)]
+pub struct Scrape {
+    seeders:   i32,
+    completed: i32,
+    leechers:  i32,
+}
+
+#[derive(Debug)]
 pub struct Client {
-    c_type:       String,
-    info_hash:    String,
-    tracker_addr: SocketAddrV4,
+    c_type:         String,
+    info_hash:      Vec<String>,
+    tracker_addr:   String,
+    connected:      bool,
+    action:         Option<i32>,
+    transaction_id: Option<Vec<u8>>,
+    connection_id:  Option<Vec<u8>>,
+}
+
+enum AnnounceError {
+    UdpSend(UdpSendError),
+}
+
+enum ScrapeError {
+    UdpSend(UdpSendError),
+}
+
+enum ConnectionError {
+    UdpSend(UdpSendError),
+}
+
+enum UdpSendError {
+    NoResponse,
 }
 
 impl Client {
-    pub fn new(c_type: &str, info_hash: &str, tracker_addr: &str) -> Client {
+    pub fn new(c_type: &str, info_hash: Vec<String>, tracker_addr: &str) -> Client {
         Client {
-            c_type,
-            info_hash,
-            tracker_addr,
+            c_type:         c_type.to_string(),
+            info_hash:      info_hash,
+            tracker_addr:   tracker_addr.to_string(),
+            connected:      false,
+            action:         None,
+            transaction_id: None,
+            connection_id:  None,
         }
     }
 
-    pub fn get_connection_key() {
+    pub fn announce(&self) -> Result<(i32, i32, i32, Vec<Peer>), AnnounceError> {
+        if self.connected == false {
+            self.connection_request();
+        }
 
+        // TODO: Set an actual tx_id
+        let buf = [&self.connection_id.unwrap(), ACTION_ANNOUNCE, &self.transaction_id.unwrap(), ].concat();
+        let (res_size, res) = self.udp_send(&buf).unwrap_err(AnnounceError::UdpSend);
+
+        let action         = Cursor::new(&res[0..4]).read_i32::<BigEndian>().unwrap();
+        let transaction_id = res[4..8].to_vec();
+        let intervals      = Cursor::new(&res[8..12]).read_i32::<BigEndian>().unwrap();
+        let leechers       = Cursor::new(&res[12..16]).read_i32::<BigEndian>().unwrap();
+        let seeders        = Cursor::new(&res[16..20]).read_i32::<BigEndian>().unwrap();
+
+        let size = res_size - 20;
+        let scrapes: Vec<Peer> = Vec::new();
+
+        res[20..].chunk().map(|x| {
+            let ip = Ipv4Addr::new(x[0], x[1], x[2], x[3]);
+            let port = (x[4] as u16) * 256 + (x[5] as u16);
+            scrapes.push(Peer { ip, port });
+        });
+
+        Ok((intervals, leechers, seeders, scrapes))
     }
 
-    pub fn announce() {
+    pub fn scrape(&self) -> Result<Vec<Scrape>, ScrapeError> {
+        if self.connected == false {
+            self.connection_request();
+        }
 
+        // TODO: Set an actual tx_id
+        let buf = [&self.connection_id.unwrap(), ACTION_SCRAPE, &[0, 0, 13, 1], &self.info_hash.iter().map(|x| x.as_bytes().to_vec()).collect::<Vec<Vec<u8>>>().concat()].concat();
+        let (res_size, res) = self.udp_send(&buf).unwrap_err(ScrapeError::UdpSend);
+
+        let action         = Cursor::new(&res[0..4]).read_i32::<BigEndian>().unwrap();
+        let transaction_id = res[4..8].to_vec();
+
+        let size = res_size - 8;
+        // let scrapes: Vec<Scrape> = Vec::new();
+        res[8..].chunk(12).map(|x| {
+            Scrape {
+                seeders: x[0..4],
+                completed: x[4..8],
+                leechers: x[8..16]
+            }
+        })
     }
 
-    pub fn scrape(&self) {
+    fn connection_request(&mut self) -> Result<(), ConnectionError> {
+        // TODO: Set an actual tx_id
+        let buf = [PROTOCOL_ID, ACTION_CONNECT, &[0, 0, 13, 1]].concat();
+        let (res_size, res) = match self.udp_send(&buf) {
+            Ok((s, r)) => (s, r),
+            Err(e) => Err(ConnectionError::UdpSend),
+        };
 
+        self.action         = Some(Cursor::new(&res[0..4]).read_i32::<BigEndian>().unwrap());
+        self.transaction_id = res[4..8].to_vec();
+        self.connection_id  = res[8..16].to_vec();
+
+        self.connected = true;
+
+        Ok(())
     }
 
-    pub fn connection_request(&self) -> Result<(usize, SocketAddr)> {
-        let socket = UdpSocket::bind("127.0.0.1:34254").expect("couldn't bind to address");
-        socket.send_to([PROTOCOL_ID, ACTION_CONNECT, [0x00, 0x00, 0x13, 0x01]].concat(), self.tracker_addr).expect("couldn't send data");
-        let mut buf = [0; 100];
-        (number_of_bytes, src_addr) = socket.recv_from(&mut buf).expect("Didn't receive data")
-
+    fn udp_send(&self, buf: &[u8]) -> Result<(usize, Vec<u8>), UdpSendError> {
+        let socket = UdpSocket::bind("0.0.0.0:34254").expect("couldn't bind to address");
+        socket.send_to(&buf, &self.tracker_addr).expect("couldn't send data");
+        let mut buf = [0; 256];
+        let size: usize;
+        loop {
+            let (s, a) = socket.recv_from(&mut buf).unwrap();
+            if s > 0 {
+                size = s;
+            }
+        }
+        Ok((size, buf.to_vec()))
     }
 }
 
